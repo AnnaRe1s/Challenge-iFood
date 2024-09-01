a7905c8f4e924246ba63a3cc9538376b3c495402
Anna Reis <reis.annabeatriz@outlook.com>
2024-09-01 17:40:09 -0300

    :sparkles: feat: Estrutura cadastro do usuario :lipstick: feat: estilo cadastro do usuario

--- js/index.js
+++ js/index.js
@@ -5,4 +5,28 @@ setTimeout(() => {
   logo.style.opacity = "0";
   logo.style.display = "none";
   loginForm.style.display = "flex";
-}, 2000);
+}, 5000);
+
+// Alteracao de tela para cadastro e login
+const btnLogin = document.getElementById("btn-login");
+const btnCadastro = document.getElementById("btn-register");
+const conteudo = document.getElementById("conteudo");
+
+btnLogin.addEventListener("click", () => {
+  loginForm.style.display = "none";
+  fetch("login.html")
+    .then((response) => response.text())
+    .then((data) => {
+      conteudo.innerHTML = data;
+      loginForm.style.display = "none";
+    });
+});
+
+btnCadastro.addEventListener("click", () => {
+  loginForm.style.display = "none";
+  fetch("register.html")
+    .then((response) => response.text())
+    .then((data) => {
+      conteudo.innerHTML = data;
+    });
+});
