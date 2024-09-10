6ee64f707341db239cfa62316820917435463491
Felipe Dunder <felipedunder@gmail.com>
2024-09-10 19:37:51 -0300

    Tx Entrega_Final

--- js/txEntrega.js
+++ js/txEntrega.js
@@ -70,9 +70,6 @@ document.addEventListener("DOMContentLoaded", function () {
             // Atualize com os dados para 1 ano
         }
     }
-});
-
-document.addEventListener('DOMContentLoaded', function () {
     const modal = document.getElementById('modal');
     const abrirModalBtn = document.getElementById('abrirModal');
     const fecharModalBtn = document.querySelector('.fechar');
@@ -87,10 +84,9 @@ document.addEventListener('DOMContentLoaded', function () {
         modal.style.display = 'none';
     });
 
-    // Fecha o modal ao clicar fora dele
-    window.addEventListener('click', function (event) {
-        if (event.target == modal) {
-            modal.style.display = 'none';
-        }
+    saveButton.addEventListener('click', function () {
+        modal.style.display = 'none'; // Fecha o modal após salvar
     });
+
+
 });
